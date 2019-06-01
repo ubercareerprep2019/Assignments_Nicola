70279d2397e47a7d45559481f9376df8d85991ac
Nicola Ramdass <50161620+nicolaramdass@users.noreply.github.com>
2019-06-01 16:27:53 -0400

    Update LinkedLists.py

--- LinkedLists.py
+++ LinkedLists.py
@@ -5,36 +5,71 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.first = None
-        self.last = None
+        self.dummyhead = None
+        self.dummytail = None
+        self.pre = Node()
+        self.pre.value = None
+        self.pre.next = self.dummytail
+
     def pushBack(self, value):
         current = Node()
         current.value = value
-        current.next = None
-        self.last.next = current
-        self.last = current
+        current.next = self.dummytail
+        if self.pre is None:
+            self.pre = current
+            self.dummyhead.next = self.pre
+        else:
+            self.pre.next = current
+            self.pre = current
+
     def popBack(self):
-        return self.last.value.pop()
+        node = self.dummyhead.next
+        value = self.pre.value
+        while node != self.pre:
+            node = node.next
+        node.next = self.dummytail
+        self.pre = node
+        return value
+
     def insert(self, index, value):
-        if index > LinkedList.size:
+        if index >= LinkedList.size:
             return
         else:
-            node = self.first
+            node = self.dummyhead.next
             previous = None
             i = 0
-            while i != index:
+            while i != index and node is not self.dummytail:
                 previous = node
                 node = node.next
+                i += 1
             current = Node()
             current.value = value
             current.next = node
             previous.next = current
-    def erase(self):
-        
+
+    def erase(self, index):
+        node = self.dummyhead.next
+        previous = None
+        i = 0
+        while i != index and node is not self.dummytail:
+            previous = node
+            node = node.next
+            i += 1
+        previous.next = node.next
+
+    def elementAt(self, index):
+        node = self.dummyhead.ext
+        i = 0
+        while i != index and node is not self.dummytail:
+            node = node.next
+            i += 1
+        return node.value
+    
     def size(self):
-        node = self.first
+        node = self.dummyhead.next
         i = 0
         while node is not None:
             node = node.next
             i += 1
         return i
+
