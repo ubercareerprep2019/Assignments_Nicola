87680153b4a2003adc7d2f287787c840a74cc1e1
Nicola Ramdass <50161620+nicolaramdass@users.noreply.github.com>
2019-07-22 21:44:37 -0400

    Update Part1.py

--- Assignment-2/Part1.py
+++ Assignment-2/Part1.py
@@ -10,6 +10,7 @@ class TreeNode:
             self.right = right
         self.data = data
 
+
     # Trees - Ex1
     def PrintTree(self):
         print(self.data)
@@ -53,3 +54,38 @@ class OrganizationStructure:
             boss = queue.pop(0)
             if boss.directReports is not None:
                 queue.append(boss.directReports)
+
+    # Trees - Ex3
+    def printNumLevels(self, leaf):
+        if leaf is None:
+            return 0
+        maxdepth = 0
+        for  i in leaf.directReports:
+            if maxdepth < i.printNumLevels:
+                maxdepth = i.printNumLevels
+        return maxdepth
+
+
+
+company = OrganizationStructure("A", "CEO")
+cfo = Employee("B", "CFO")
+cto = Employee("C", "CTO")
+managerD = Employee("D", "Manager")
+managerE = Employee("E", "Manager")
+engineerF = Employee("F", "Engineer")
+engineerG = Employee("G", "Engineer")
+engineerH = Employee("H", "Engineer")
+director = Employee("I", "Director")
+salesRep = Employee("J", "Sales Representative")
+salesIntern = Employee("K", "Sales Intern")
+
+company.ceo.directReports = [cfo, cto]
+cto.directReports = [managerD, managerE]
+managerD.directReports = [engineerF, engineerG, engineerH]
+cfo.directReports = [director]
+director.directReports = [salesRep]
+salesRep.directReports = [salesIntern]
+
+company.PrintByLevel()
+
+
