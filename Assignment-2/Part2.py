0fb612c38610cc95e0191813a61a25dc1f58b727
Nicola Ramdass <50161620+nicolaramdass@users.noreply.github.com>
2019-07-22 22:17:51 -0400

    Update Part2.py

--- Assignment-2/Part2.py
+++ Assignment-2/Part2.py
@@ -1,1 +1,30 @@
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
 
+
+def find(node, key):
+    if node is None or node.data == key:
+        return node
+    if node.data < key:
+        return find(node.right, key)
+    if node.data > key:
+        return find(node.left, key)
+
+
+def insert(node, keyNode):
+    if node is None:
+        node = keyNode
+    else:
+        if node.data < keyNode.data:
+            if node.right is None:
+                node.right = keyNode
+            else:
+                insert(node.right, keyNode)
+        else:
+            if node.left is None:
+                node.left = keyNode
+            else:
+                insert(node.left, keyNode)
